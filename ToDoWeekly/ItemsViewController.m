dfbba15c691577f32b6ed8736d5ed170255ffdf2
Jay <10294167+jayjson@users.noreply.github.com>
2023-08-17 18:25:27 -0400

    added a basic table view in the items screen

--- ToDoWeekly/ItemsViewController.m
+++ ToDoWeekly/ItemsViewController.m
@@ -1,6 +1,8 @@
 #import "ItemsViewController.h"
 
-@interface ItemsViewController ()
+@interface ItemsViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) UITableView *tableView;
 
 @end
 
@@ -8,8 +10,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = UIColor.systemMintColor;
+    
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)
+                                                  style:UITableViewStylePlain];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
 }
 
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"itemCell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"itemCell"];
+    }
+    cell.textLabel.text = @"Hell world";
+    return cell;
+}
 
 @end
