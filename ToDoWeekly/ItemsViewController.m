3881b8e7feb329254b03930096855ad670e11c46
Jay <10294167+jayjson@users.noreply.github.com>
2023-08-17 20:13:26 -0400

    implemented the add bar button and the add alert

--- ToDoWeekly/ItemsViewController.m
+++ ToDoWeekly/ItemsViewController.m
@@ -16,6 +16,31 @@
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     [self.view addSubview:self.tableView];
+    
+    [self configureNavBar];
+}
+
+- (void)configureNavBar {
+    self.navigationItem.title = @"My To Do's";
+    UIImage *image = [UIImage systemImageNamed:@"plus"];
+    UIBarButtonItem *plusBarButton = [[UIBarButtonItem alloc] initWithImage:image style:UIBarButtonItemStylePlain target:self action:@selector(handleTapOnPlusButton)];
+    self.navigationItem.rightBarButtonItem = plusBarButton;
+}
+
+- (void)handleTapOnPlusButton {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Add a new to do item" message:@"" preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"Describe the new task";
+    }];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+    UIAlertAction *saveAction = [UIAlertAction actionWithTitle:@"Save" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        // TODO: Handle save request
+    }];
+    [alertController addAction:cancelAction];
+    [alertController addAction:saveAction];
+    
+    [self presentViewController:alertController animated:true completion:nil];
 }
 
 - (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
